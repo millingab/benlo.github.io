ac82538ef462799b64f9e99b2c0ed2dff1cf76fe
Millin Gabani <gabanimillin@gmail.com>
2023-03-05 22:18:50 -0500

    change images

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ btn.addEventListener("click", (e) => {
 });
 
 function preload() {
-  imgNum = Math.floor(Math.random() * 21);
+  imgNum = Math.floor(Math.random() * 100) % 16;
   img = loadImage(
     "https://raw.githubusercontent.com/millingab/benlo.github.io/main/" +
       String(imgNum) +
