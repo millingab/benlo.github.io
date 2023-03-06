89dc55534d50c5537af46ec6644f0edd2e54c583
Millin Gabani <gabanimillin@gmail.com>
2023-03-05 20:16:46 -0500

    Add images

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ btn.addEventListener("click", (e) => {
 
 function preload() {
   img = loadImage(
-    "https://images.unsplash.com/photo-1677741447337-48aba59a8f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
+    "https://raw.githubusercontent.com/millingab/benlo.github.io/main/0-2.png"
   );
 }
 
